089cf38a433edff8c997f7aa9f7e80fa00d5b1f9
Seval <aslanseval1986@gmail.com>
2024-02-04 13:56:35 +0000

    Day1-15

---
+++ IdeaProjects/GitGithub/src/gitGithub/Class01.java
@@ -0,0 +1,11 @@
+package gitGithub;
+
+public class Class01 {
+    public static void main(String[] args) {
+
+        int a = 43;
+        int b = 44;
+
+        System.out.println("a + b = " + (a + b));
+    }
+}
